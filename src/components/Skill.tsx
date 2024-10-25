5584dae082b22d55c4a4dcaea6d1a48b622c438d
m0nztrum <delafiado@gmail.com>
2024-10-25 23:50:12 +0000

    animations

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -5,20 +5,34 @@ import { motion } from 'framer-motion';
 export const Skill = () => {
     const skillIconItems = currentTech;
     return (
-        <div className="relative py-16 sm:py-24" id="skills z-10">
+        <div className="relative z-10 py-16 sm:py-24" id="skills">
             <div className="mb-10 space-y-4">
-                <h1 className="text-3xl font-bold min-[430px]:text-4xl md:text-5xl dark:text-stone-200">
+                <motion.h1
+                    initial={{ opacity: 0, x: -70 }}
+                    whileInView={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 0.5, delay: 0.25 }}
+                    viewport={{ once: true }}
+                    className="text-3xl font-bold min-[430px]:text-4xl md:text-5xl dark:text-stone-200"
+                >
                     Current Technologies
-                </h1>
-                <p className="text-sm">
+                </motion.h1>
+                <motion.p
+                    initial={{ opacity: 0, x: -90 }}
+                    whileInView={{ opacity: 1, x: 0 }}
+                    viewport={{ once: true }}
+                    transition={{ duration: 0.5, delay: 0.25 }}
+                    className="max-w-lg text-sm text-dark-500 md:max-w-3xl"
+                >
                     I am proficient in a range of tools and technologies that that empower me to
                     build my projects.
-                </p>
+                </motion.p>
             </div>
+
             <motion.div
-                initial={{ opacity: 0, y: 70 }}
+                initial={{ opacity: 0, y: 75 }}
                 whileInView={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.5 }}
+                transition={{ duration: 0.5, delay: 0.25 }}
+                viewport={{ once: true }}
                 className="grid grid-cols-1 items-center justify-between gap-4 sm:grid-cols-2 lg:grid-cols-4"
             >
                 {skillIconItems.map((skillIconItem) => (
