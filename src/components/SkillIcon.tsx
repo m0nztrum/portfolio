606f96e4a2e961693dc84f7bce2ddb78337385e4
m0nztrum <delafiado@gmail.com>
2024-10-25 18:06:03 +0000

    skill section

--- src/components/SkillIcon.tsx
+++ src/components/SkillIcon.tsx
@@ -4,9 +4,9 @@ export const SkillIcon = ({
     techInfo: { name: string; description: string; imageUrl: string; bgColor: string };
 }) => {
     return (
-        <div>
-            <div>
-                <img src={techInfo.imageUrl} alt={`${techInfo.name} logo`} />
+        <div className="flex flex-1 gap-5 rounded-xl border border-dark-500 p-2.5">
+            <div className={`w-fit rounded-lg p-3`}>
+                <img src={techInfo.imageUrl} alt={`${techInfo.name} logo`} className="size-8" />
             </div>
             <div>
                 <h4 className="text-lg font-medium">{techInfo.name}</h4>
