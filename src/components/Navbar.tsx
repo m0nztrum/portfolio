4b12afb4b831d9a6c15796c9b6ff24c3d40648c2
m0nztrum <delafiado@gmail.com>
2024-10-26 11:29:58 +0000

    navbar link fixes (mobile)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,20 +2,22 @@ import { navItems } from '../data';
 import { navItem } from '../types';
 export const Navbar = () => {
     return (
-        <div className="fixed inset-x-0 top-10 z-[5000] mx-auto flex max-w-fit items-center justify-center space-x-4 rounded-lg border border-dark-500 bg-dark-200 px-8 py-4">
-            {navItems.map((navItem: navItem, index: number) => (
-                <nav
+        <nav className="fixed inset-x-0 top-10 z-[5000] mx-auto flex max-w-fit items-center justify-center space-x-4 rounded-lg border border-dark-500 bg-dark-200 px-8 py-4">
+            {navItems.map((item: navItem, index: number) => (
+                <div
                     key={index}
                     className="relative flex items-center space-x-1 text-neutral-50 hover:text-neutral-300"
                 >
                     {/* for mobile screen sizes */}
-                    <a className="block sm:hidden">{navItem.icon}</a>
-                    {/* for larger screen sizes */}
-                    <a href={navItem.link} className="hidden font-medium sm:block">
-                        {navItem.name}
+                    <a href={item.link} className="block sm:hidden">
+                        {item.icon}
                     </a>
-                </nav>
+                    {/* for larger screen sizes */}
+                    <a href={item.link} className="hidden font-medium sm:block">
+                        {item.name}
+                    </a>
+                </div>
             ))}
-        </div>
+        </nav>
     );
 };
