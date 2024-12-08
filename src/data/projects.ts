616989254ffb9a90dd9a9927cb1dbe42527fce34
m0nztrum <delafiado@gmail.com>
2024-12-08 10:13:56 +0000

    edited projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -22,11 +22,37 @@ export const projects = [
     },
     {
         id: 2,
-        title: 'crowdspark',
+        title: 'crowdspark (🔨 WIP)',
         description: 'A simple web app to manange events and Q&A sessions.',
         imageUrl: 'images/projects/wip.webp',
         projectLink: '',
         codeLink: 'https://github.com/m0nztrum/crowdspark',
+        tags: [
+            { icon: 'images/techIcons/react.svg', tagName: 'React', tagColor: 'bg-[#61DAFB]/20' },
+            {
+                icon: 'images/techIcons/tailwind.svg',
+                tagName: 'Tailwind',
+                tagColor: 'bg-[#0EA5E9]/20',
+            },
+            {
+                icon: 'images/techIcons/typescript.svg',
+                tagName: 'TypeScript',
+                tagColor: 'bg-[#007ACC]/20',
+            },
+            {
+                icon: 'images/techIcons/supabase.svg',
+                tagName: 'Supabase',
+                tagColor: 'bg-[#610]/20',
+            },
+        ],
+    },
+    {
+        id: 3,
+        title: 'agricart (🔨 WIP)',
+        description: 'An E-commerce shop selling farm products',
+        imageUrl: 'images/projects/wip.webp',
+        projecLink: '',
+        codeLink: 'https://github.com/m0nztrum/agricart',
         tags: [
             { icon: 'images/techIcons/react.svg', tagName: 'React', tagColor: 'bg-[#61DAFB]/20' },
             {
